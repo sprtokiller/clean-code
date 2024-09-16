79b5f8816a22e384681e12de9b3b36650dd10b24
Jan Havlíček <jan.havlicek@student.sps-cl.cz>
2024-09-16 12:26:09 +0200

    JavaScript ulohy 1-2

--- JavaScript/3/3_1.js
+++ JavaScript/3/3_1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const MINIMUM_YEARS_OF_SERVICE = 10
 
 if (employee.yearsOfService > MINIMUM_YEARS_OF_SERVICE) {
@@ -6,10 +5,3 @@ if (employee.yearsOfService > MINIMUM_YEARS_OF_SERVICE) {
 } else {
   console.log("Not eligible for award.");
 }
-=======
-if (employee.yearsOfService > 10) {
-  console.log("Eligible for long-term service award.");
-} else {
-  console.log("Not eligible for award.");
-}
->>>>>>> 68d80e5e0a16bd7bf800e7e34a8da514ef5bf4dd
