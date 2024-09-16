0a460d90296c9bb52a80c79b78d2434a2fc74f70
jakubsps6 <jakub.zeman@student.sps-cl.cz>
2024-09-16 11:32:53 +0200

    Hotovo 1_1.js

--- JavaScript/1/1_1.js
+++ JavaScript/1/1_1.js
@@ -1,10 +1,7 @@
-function c(a, b) {
-  let d = Math.sqrt(a * a + b * b);
-  console.log("The result is: " + d);
-  return d;
+function vypocet(a, b) {
+  let odmocnina = Math.sqrt(a * a + b * b);
+  console.log("The result is: " + odmocnina);
+  return odmocnina;
 }
 
-let x = 3;
-let y = 4;
-let r = c(x, y);
-console.log("Computed value: " + r);
+vypocet(3, 4);
