14909486b5b16462207deb498dd4a776fc98f374
= <=>
2024-09-16 12:39:33 +0200

    Remove all unnecessary comments

--- JavaScript/2/2_4.js
+++ JavaScript/2/2_4.js
@@ -1,4 +1,2 @@
-// Set the background color to green
 document.body.style.backgroundColor = "green";
-// Display the success message
 document.getElementById("status").innerText = "Operation successful.";
