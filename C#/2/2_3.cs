82431543bb0594e110a1f3758cfd054006239036
3.D Kreisinger Jindřich <Jindrich.Kreisinger@student.sps-cl.cz>
2024-09-16 11:53:51 +0200

    úloha 2

--- C#/2/2_3.cs
+++ C#/2/2_3.cs
@@ -1,8 +1,6 @@
-// Loop through the array of numbers
+// Get value of each index in array and print it
 for (int i = 0; i < numbers.Length; i++)
 {
-    // Get the current number
     int num = numbers[i];
-    // Print the number
     Console.WriteLine("Number: " + num);
 }
