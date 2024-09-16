97bca8c766c1a9cb5365191abab484ea2eb77518
3.E Weich Jakub <Jakub.Weich@student.sps-cl.cz>
2024-09-16 12:09:33 +0200

    C# task group 1 completed

--- C#/1/1_2.cs
+++ C#/1/1_2.cs
@@ -4,15 +4,15 @@ class Program
 {
     static void Main()
     {
-        int[] arr = { 5, 10, 15, 20, 25 };
-        p(arr);
+        int[] multiplesOfFive = { 5, 10, 15, 20, 25 };
+        getIndexValue(multiplesOfFive);
     }
 
-    static void p(int[] a)
+    static void getIndexValue(int[] userArray)
     {
-        for (int i = 0; i < a.Length; i++)
+        for (int i = 0; i < userArray.Length; i++)
         {
-            Console.WriteLine("Value at index " + i + ": " + a[i]);
+            Console.WriteLine("Value at index " + i + ": " + userArray[i]);
         }
     }
 }
