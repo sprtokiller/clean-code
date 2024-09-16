863281c55037ef63f5feebb625b3c5b752517bbd
JanBulir <147809266+JanBulir@users.noreply.github.com>
2024-09-16 12:17:02 +0200

    Add files via upload
    JavaScript(2)

--- JavaScript/2/2_1.js
+++ JavaScript/2/2_1.js
@@ -1,9 +1,9 @@
-// This function multiplies two numbers
+/**
+ * Multiplies two numbers and returns the product.
+ */
 function multiply(a, b) {
-  // Multiply a and b
   return a * b;
 }
 
-// Call the multiply function with 4 and 5
 let product = multiply(4, 5);
 console.log("Product is: " + product);
