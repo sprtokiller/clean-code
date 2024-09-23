53960998bbb2d42499a1fa93221327da174388aa
Kryštof Háva <Krystof.Hava@hotmail.com>
2024-09-23 11:49:56 +0200

    Fixed poor code formating

--- JavaScript/7/7_4.js
+++ JavaScript/7/7_4.js
@@ -1,11 +1,12 @@
-function divide(a,b){
-  if(b!==0){
-  return a/b;}else{
-  console.error("Division by zero.");
-  return null;
+function divide(a, b) {
+  if (b !== 0) {
+    return a / b;
   }
+  else {
+    console.error("Division by zero.");
+    return null;
   }
+}
   
-  let result=divide(10,2);
-  console.log("Result:"+result);
-  
+let result = divide(10, 2);
+console.log(`Result:" ${result}`);
