a68eddcc690a04a8b2fbbc7e1f9aa0a1535ea8f5
3.D Kreisinger Jindřich <Jindrich.Kreisinger@student.sps-cl.cz>
2024-09-23 12:11:20 +0200

    úloha 7, podúloha 1

--- C#/7/7_1.cs
+++ C#/7/7_1.cs
@@ -1,11 +1,14 @@
 double CalculateAverage (double[] numbers){
-double total=0;
-for(int i=0;i<numbers.Length;i++){
-total+=numbers[i];
-}
-double average=total/numbers.Length;
-return average;}
 
-double[] scores={80,90,100};
-double avgScore=CalculateAverage(scores);
-Console.WriteLine("Average Score: "+avgScore);
+    double total = 0;
+    for(int i = 0; i < numbers.Length ; i++)
+        {
+    total += numbers[i];
+        }
+    double average = total / numbers.Length;
+    return average;
+}
+
+double[] scores = {80, 90, 100};
+double avgScore = CalculateAverage(scores);
+Console.WriteLine("Average Score: " + avgScore);
