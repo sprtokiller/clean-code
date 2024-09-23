c3e7292d8c16f4ce3bf08b0a7f85fc42a9303652
3.E Virth Viktor <Viktor.Virth@student.sps-cl.cz>
2024-09-23 12:24:49 +0200

    2_1 - 2_4

--- C#/2/2_1.cs
+++ C#/2/2_1.cs
@@ -2,9 +2,15 @@ using System;
 
 class Program
 {
+    // Funkce pro výpočet produktu
     static void Main()
     {
         int product = Multiply(4, 5);
         Console.WriteLine("Product is: " + product);
+    }
+
+    static int Multiply(int a, int b)
+    {
+        return a * b;
     }
 }
