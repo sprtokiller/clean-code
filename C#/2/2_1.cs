6b1ef9fecec6e7630227a62c051178e9f068dd7d
3.E Virth Viktor <Viktor.Virth@student.sps-cl.cz>
2024-09-23 12:08:56 +0200

    Celá úloha 2 upravena

--- C#/2/2_1.cs
+++ C#/2/2_1.cs
@@ -2,15 +2,9 @@ using System;
 
 class Program
 {
-    // logické operace, násobení
     static void Main()
     {
         int product = Multiply(4, 5);
         Console.WriteLine("Product is: " + product);
-    }
-    
-    static int Multiply(int a, int b)
-    {
-        return a * b;
     }
 }
